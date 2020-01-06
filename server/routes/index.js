dddf314eedba24e7dd750394aedd558e1e58f139
Peter Morgan <peter@1080peter.com>
2020-01-06 10:12:41 -0700

    Fixed index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,5 @@
 const routes = require('express').Router();
-<<<<<<< HEAD:routes/index.js
-const apiV1 = require('./api/v1.js');
-=======
 const apiV1 = require('./api/v1');
->>>>>>> a232882774fdb412227932c43297ac164b89e97a:server/routes/index.js
 routes.use('/api', apiV1);
 
 routes.get("/", (request, response) => {
