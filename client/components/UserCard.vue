c52fc06fb52e9acd31ae0aa3225b4aafbc3f99a6
Christian Martin <ct-martin@users.noreply.github.com>
2020-02-08 17:29:51 -0500

    Use router link for profile card

--- client/components/UserCard.vue
+++ client/components/UserCard.vue
@@ -3,16 +3,16 @@
         <div class="card-img-top" style="position: relative;">
             <img :src="user.avatar_url" :alt="user.avatar_url" class="img-fluid"/>
             <div class="actions">
-                <a :href="`/friends-graph/${user.login}`" class="btns" title="View Friends Graph">
+                <nuxt-link :to="`/friends-graph/${user.login}`" class="btns" title="View Friends Graph">
                     <svg viewBox="0 0 24 24">
                         <path d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
                     </svg>
-                </a>
-                <a :href="`/timeline-graph/${user.login}`" class="btns" title="View Timeline Graph">
+                </nuxt-link>
+                <nuxt-link :to="`/timeline-graph/${user.login}`" class="btns" title="View Timeline Graph">
                     <svg viewBox="0 0 24 24">
                         <path d="M2,2H4V20H22V22H2V2M7,10H17V13H7V10M11,15H21V18H11V15M6,4H22V8H20V6H8V8H6V4Z" />
                     </svg>
-                </a>
+                </nuxt-link>
             </div>
         </div>
         <div class="card-body">
