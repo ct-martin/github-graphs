4f35c9892c8487cf491689ac29bbff3b1a6e88c7
Christian Martin <ct-martin@users.noreply.github.com>
2020-02-08 13:43:01 -0500

    Add footer

---
+++ client/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="container-fluid bg-dark">
+        <div class="container">
+            <ul>
+                <li class="nav-item">
+                    <nuxt-link class="nav-link" to="/graph-generator">Generate graphs</nuxt-link>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://github.com/jrtechs/github-graphs/">View on GitHub</a>
+                </li>
+                <li class="nav-item">
+                    <nuxt-link class="nav-link" to="/about">About</nuxt-link>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</template>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -5,14 +5,18 @@
       <div class="main">
           <nuxt/>
       </div>
+
+      <page-footer/>
   </div>
 </template>
 
 <script>
+import PageFooter from '~/components/Footer.vue'
 import Navbar from '~/components/Navbar.vue'
 
 export default {
   components: {
+    PageFooter,
     Navbar
   }
 }
