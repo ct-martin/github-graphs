4e86df9f18eb31c54bcf8b3c524d27236ddc4e0f
Aidan <34166033+The-Doctor-Of-11@users.noreply.github.com>
2020-02-08 17:28:36 -0500

    Refactor footer (#4)
    * Adding Footer
    
    * Added footerstyling, bootstrap
    
    * Fixing PackageLock
    
    * Added NewLine PackageLock

--- client/components/Footer.vue
+++ client/components/Footer.vue
@@ -1,17 +1,17 @@
 <template>
-    <footer class="container-fluid bg-dark">
-        <div class="container">
-            <ul>
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" to="/graph-generator">Generate graphs</nuxt-link>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="https://github.com/jrtechs/github-graphs/">View on GitHub</a>
-                </li>
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" to="/about">About</nuxt-link>
-                </li>
-            </ul>
+    <footer class="footer row">
+        <div class="col-sm">
+            <a  href="https://github.com/jrtechs/github-graphs">
+                <img class="footer-img" src="/img/github-circle-light.png">
+            </a>
+        </div>
+        <div class="col-sm">
+            <h6 style="color: white;">This project is registered under Mozilla Public License v2.0. You can find more information on this license <a href="http://mozilla.org/MPL/2.0/">here</a>.</h6>
+        </div>
+        <div class="col-sm">
+            <a href="mailto: &#106;&#101;&#102;&#102;&#101;&#114;&#121;&#64;&#106;&#114;&#116;&#101;&#99;&#104;&#115;&#46;&#110;&#101;&#116;" target="_blank">
+                <button class="btn btn-tert" style="margin: 0;">Contact</button>
+            </a>
         </div>
     </footer>
 </template>
